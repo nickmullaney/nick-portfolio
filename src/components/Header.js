2ce3ab449cb1cc9d0e4a98d54357a5f76b415626
nickmullaney <nick.mullaney@gmail.com>
2023-01-21 15:53:38 -0800

    fin al update

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ class Header extends Component {
             <Switch 
               checked={this.state.checked}
               onChange={this.onThemeSwitchChange}
-              offColor="#baaa80"
+              offColor="#1D7373"
               onColor="#353535"
               className="react-switch mx-auto"
               width={90}
